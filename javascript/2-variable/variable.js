80c30f34b472252a60076067ad957ad154243919
james <jaamesmendoza@gmail.com>
2026-03-29 23:57:45 +0800

    add some learning files

--- javascript/2-variable/variable.js
+++ javascript/2-variable/variable.js
@@ -5,17 +5,20 @@
 
 
 // console
-let myName = "James";
+let myName = "James"; //string
     console.log(`my name is ${myName}`);
 
-let price = 200;
+let price = 200; //number
     console.log(`the currently price of my bag is ${price}`);
 
-let grade = 1.26;
+let grade = 1.26; //float
     console.log(`my grade in my programming subject is ${grade}`);
 
-let fact = true;
+let fact = true; //boolean
     console.log(`philipine's goverment is full of corrupt politicians. ${fact}`);
+
+let mariella = false; //boolean
+    console.log(`i think mariella is a good person. ${mariella}`);
 
 let apple = 10;
 let orange = 15;
@@ -30,7 +33,11 @@ let total = apple + orange;
 let fullname = "James Mendoza";
 let age = 20;
 let height = 154.33;
+let allowance = 500 + 500;
+let totalAllowance = allowance + 200;
 
     document.getElementById('myName').textContent = `my name is ${fullname}`;
     document.getElementById('myAge').textContent = `my age is ${age}`;
     document.getElementById('myHeight').textContent = `my heigh is also ${height}`;
+    document.getElementById('allowance').textContent = `my allowance every 2 weeks is ${allowance}`;
+    document.getElementById('totalAllowance').textContent = `my total allowance is ${totalAllowance} if i include my transportation allowance.`;
